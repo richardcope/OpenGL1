18e819e75d71d2252fbd8133665e88d438d15823
richardcopei7213034 <richardcope1995@gmail.com>
2015-01-13 17:09:13 +0000

    Added cube function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <SDL.h>
 #include <cstdlib>
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+void drawTriangle();
+void drawCube();
 
 int main()
 {
@@ -23,6 +28,89 @@ int main()
 
                 );
 
-    SDL_Delay(1000);
+    SDL_GLContext glContext;
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    glContext=SDL_GL_CreateContext(window);
+    //now make this the active context
+    SDL_GL_MakeCurrent( window, glContext);
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glClear( GL_COLOR_BUFFER_BIT);
+    glMatrixMode(GL_PROJECTION);
+    gluPerspective(45.0f, (float)screenSize.w/screenSize.h,
+                   0.5, 100
+                   );
+
+    glMatrixMode(GL_MODELVIEW);
+    gluLookAt(2,2,2,0,0,0,0,1,0);
+    glEnable(GL_DEPTH_TEST);
+
+    bool quit=false;
+    SDL_Event event;
+    while(!quit)
+    {
+        while(SDL_PollEvent(&event))
+        {
+            switch( event.type)
+            {
+                case SDL_QUIT : quit = true; break;
+                case SDL_KEYDOWN :
+                    switch(event .key .keysym .sym)
+                    {
+                        case SDLK_ESCAPE : quit = true; break;
+                        case SDLK_w : glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); break;
+                        case SDLK_s : glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); break;
+                    }
+            }
+        }
+
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+    drawCube();
+    SDL_GL_SwapWindow(window);
+    }
+}
+
+void drawTriangle()
+{
+    static int rot=0;
+    glPushMatrix();
+        glRotated(++rot, 0, 1, 0);
+        glBegin(GL_TRIANGLES);
+            glColor3f(1.0f, 0.0, 0.0f);
+            glVertex3f(0.0f, 1.0f, 0.0f);
+            glColor3f(0.0f, 1.0f, 0.0f);
+            glVertex3f(1.0f, -1.0f, 0.0f);
+            glColor3f(1.0f, 0.0f, 1.0f);
+            glVertex3f(-1.0f, -1.0f, 0.0f);
+        glEnd();
+     glPopMatrix();
+
+}
+
+void drawCube()
+{
+    static int rot=0;
+    glPushMatrix();
+        glRotated(++rot, 0, 1, 0);
+        glBegin(GL_QUADS);
+
+            glColor3f(1.0, 0.0, 0.0);
+            glVertex3f(-1, -1, -1);
+            glVertex3f(-1, 1, 1);
+            glVertex3f(1, 1, 1);
+            glVertex3f(1, -1, 1);
+
+            glColor3f(0.0, 1.0, 0.0);
+            glVertex3f(-1, -1, -1);
+            glVertex3f(-1, 1, 1);
+            glVertex3f(-1, 1, -1);
+            glVertex3f(1, -1, 1);
+
+        glEnd();
+     glPopMatrix();
 
 }
